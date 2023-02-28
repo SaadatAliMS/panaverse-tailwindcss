d447a29e9b611a137b659c4e1d48b6f9ae653df6
SaadatAliPhd <frhnsaif@yahoo.com>
2023-02-28 23:07:48 +0500

    NextJs and TailwindCss

---
+++ app/components/layout/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      {/* logo */}
+      <h2>Panaverse Dao</h2>
+      {/* Navigation bar */}
+      <ul>
+        <li>Home</li>
+        <li>Courses</li>
+      </ul>
+    </header>
+  );
+}
