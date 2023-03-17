e00a4961f62fdcf1d713322196372fae1926c7e8
SaadatAliPhd <frhnsaif@yahoo.com>
2023-03-17 23:20:01 +0500

    change in Header

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import logo from "@/public/logo.png";
+import logo from "@/public/Logo.png";
 import Link from "next/link";
 import Wrapper from "../shared/Wrapper";
 
